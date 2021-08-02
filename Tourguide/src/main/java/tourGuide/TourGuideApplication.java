95cf3f32367dc81dd7ed63e3e70e1107ba4c70b3
Nicolas BIANCUCCI <nico5310@gmail.com>
2021-08-02 14:58:00 +0200

    Active Config Feign to Application TourGuide and Create Beans

--- Tourguide/src/main/java/tourGuide/TourGuideApplication.java
+++ Tourguide/src/main/java/tourGuide/TourGuideApplication.java
@@ -2,10 +2,12 @@ package tourGuide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import java.util.Locale;
 
 @SpringBootApplication
+@EnableFeignClients("tourGuide")
 public class TourGuideApplication {
 
     public static void main(String[] args) {
---
+++ Tourguide/src/main/java/tourGuide/beans/Provider.java
@@ -0,0 +1,56 @@
+package tourGuide.beans;
+
+import java.util.UUID;
+
+public class Provider {
+
+    private String name;
+    private double price;
+    private UUID   tripId;
+
+    public Provider(UUID tripId, String name, double price) {
+        this.name = name;
+        this.tripId = tripId;
+        this.price = price;
+    }
+
+    public Provider() {
+
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public double getPrice() {
+
+        return price;
+    }
+
+    public void setPrice(double price) {
+
+        this.price = price;
+    }
+
+    public UUID getTripId() {
+
+        return tripId;
+    }
+
+    public void setTripId(UUID tripId) {
+
+        this.tripId = tripId;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Provider{" + "name='" + name + '\'' + ", price=" + price + ", tripId=" + tripId + '}';
+    }
+}
