f55d59aa0ccbc7e227469a31a5c6a5f50d2d4df7
Nicolas BIANCUCCI <nico5310@gmail.com>
2021-08-04 08:34:15 +0200

    Complete TourGuideService interface

--- Tourguide/src/main/java/tourGuide/service/TourGuideService.java
+++ Tourguide/src/main/java/tourGuide/service/TourGuideService.java
@@ -1,6 +1,32 @@
 package tourGuide.service;
 
+import tourGuide.beans.ProviderBean;
+import tourGuide.beans.VisitedLocationBean;
+import tourGuide.dto.NearByAttractionDto;
+import tourGuide.user.User;
+import tourGuide.user.UserReward;
+
+import java.util.List;
+
 public interface TourGuideService {
+
+
+    List<UserReward> getUserRewards(User user);
+
+    VisitedLocationBean getUserLocation(User user);
+
+    User getUser(String userName);
+
+    List<User> getAllUsers();
+
+    void addUser(User user);
+
+    List<ProviderBean> getTripDeals(User user);
+
+    VisitedLocationBean trackUserLocation(User user);
+
+    List<NearByAttractionDto> getNearByAttractions(VisitedLocationBean visitedLocationBean, User user);
+
 
 
 }
