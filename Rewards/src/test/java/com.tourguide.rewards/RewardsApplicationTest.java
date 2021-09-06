f629a9bc6caf349b047186d2cb65a989469d4d2f
Nicolas BIANCUCCI <nico5310@gmail.com>
2021-09-06 10:49:51 +0200

    Complete GpsUtilTest/RewardsTest/TourGuideTest/TrippricerTest

---
+++ Rewards/src/test/java/com.tourguide.rewards/RewardsApplicationTest.java
@@ -0,0 +1,15 @@
+package com.tourguide.rewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import rewards.RewardsApplication;
+
+@SpringBootTest(classes = RewardsApplication.class)
+public class RewardsApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
---
+++ Tourguide/src/test/java/tourGuide/TourguideApplicationTest.java
@@ -0,0 +1,14 @@
+package tourGuide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = TourGuideApplication.class)
+public class TourguideApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
