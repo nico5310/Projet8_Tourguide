323c29d9378fdddfe57aae02f476319dab6532e1
Nicolas BIANCUCCI <nico5310@gmail.com>
2021-09-06 10:23:47 +0200

    Complete GpsUtil test

---
+++ GpsUtil/src/test/java/com/tourguide/gpsUtil/GpsUtilApplicationTest.java
@@ -0,0 +1,15 @@
+package com.tourguide.gpsUtil;
+
+import gpsUtil.GpsUtilApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = GpsUtilApplication.class)
+public class GpsUtilApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
